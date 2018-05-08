29f0680b55205373903b08a9a4b2d292205c3e37
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2018-05-08 07:56:31 -0600

    asd

--- includes/etc.php
+++ includes/etc.php
@@ -1,11 +1,11 @@
 
 
 <?php
-	
+
 	require ('conexion.php');
-	
+
 	echo 'Selecciona País : <select onChange="getMunicipio(this.value);" name="cbx_pais" id="cbx_pais">';
-	
+
 	//$query = "SELECT `id-pais`, `pais-nom` FROM `pst-pais` ORDER BY `pais-nom`";
 	$query = "SELECT `pais-id`, `pais-nom` FROM `pst-pais` ORDER BY `pais-nom`";
 
@@ -15,14 +15,14 @@
 	{
 		?>
 		<option value="" >Seleccione un País</option>
-		
+
 		<?php
 		while($row = $resultado->fetch_assoc())
 		{
 		?>
 
 		<option value="<?php echo $row['pais-id']; ?>"><?php echo $row['pais-nom']; ?></option>
-		
+
 		<?php
 		}
 	}
--- includes/getEstado.php
+++ includes/getEstado.php
@@ -1,12 +1,12 @@
 
 
 <?php
-	 //obtener pais 
+	 //obtener pais
 
 	require ('conexion.php');
-	
+
 	echo '<select  onChange="getMunicipio(this.value);" name="cbx_pais" id="cbx_pais" placeholder="Selecciona un País">';
-	
+
 	//$query = "SELECT `id-pais`, `pais-nom` FROM `pst-pais` ORDER BY `pais-nom`";
 	$query = "SELECT `pais-id`, `pais-nom` FROM `pst-pais` ORDER BY `pais-nom`";
 
@@ -16,13 +16,13 @@
 	{
 		?>
 		<option value="" >Seleccione un País</option>
-		
+
 		<?php
 		while($row = $resultado->fetch_assoc())
 		{
 		?>
 		<option value="<?php echo $row['pais-id']; ?>"><?php echo $row['pais-nom']; ?></option></div>
-		
+
 		<?php
 		}
 	}
